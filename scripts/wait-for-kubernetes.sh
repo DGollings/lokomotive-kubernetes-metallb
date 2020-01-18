d8e9f78075888c1691e873a4ab20522c4a1a4f00
Daniel Gollings <>
2020-01-18 15:08:07 +0100

    uses environment, easier to read

--- scripts/wait-for-kubernetes.sh
+++ scripts/wait-for-kubernetes.sh
@@ -8,12 +8,12 @@ if [ ! -f $KUBECONFIG ]; then
 fi
 
 # Wait for kubernetes to be ready before continuing
-while ! $KUBECTL --kubeconfig $KUBECONFIG get nodes 2>/dev/null | grep -v NotReady; do
+while ! $KUBECTL get nodes 2>/dev/null | grep -v NotReady; do
   # Be quiet when output is pointless
-  if $KUBECTL --kubeconfig $KUBECONFIG get nodes 2>/dev/null | grep 'no such host'; then
+  if $KUBECTL get nodes 2>/dev/null | grep 'no such host'; then
     sleep 20
   # but start announcing yourself when nodes are getting ready
-  elif $KUBECTL --kubeconfig $KUBECONFIG get nodes 2>/dev/null | grep NotReady >/dev/null; then
+  elif $KUBECTL get nodes 2>/dev/null | grep NotReady >/dev/null; then
     sleep 5 && echo Waiting for Kubernetes nodes to be ready
   fi
 done
