e9f4baea9aabe10440b57e6352362756e85a24f1
Daniel Gollings <2032823+DGollings@users.noreply.github.com>
2020-01-21 19:06:10 +0100

    updated

--- scripts/wait-for-kubernetes.sh
+++ scripts/wait-for-kubernetes.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-if [ ! -x $KUBECTL ]; then
-  echo $KUBECTL not found or executable
+if [ ! -x "$KUBECTL" ]; then
+  echo "$KUBECTL" not found or executable
   exit 1
 fi
-if [ ! -f $KUBECONFIG ]; then
-  echo $KUBECONFIG not found
-  exit 1
-fi
+for ((i = 0; i < 30; i++)); do
+  if [ ! -f "$KUBECONFIG" ]; then
+    echo "$KUBECONFIG" not found.
+    # Try for 15 minutes (30x30)
+    sleep 30
+  else
+    break
+  fi
+done
 
 # Wait for kubernetes to be ready before continuing
-while ! $KUBECTL get nodes 2>/dev/null | grep -v NotReady; do
-  # Be quiet when output is pointless
-  if $KUBECTL get nodes 2>/dev/null | grep 'no such host'; then
+while :; do
+  if ! $KUBECTL get nodes >/dev/null 2>&1; then
+    # Be quiet when output is pointless
     sleep 20
-  # but start announcing yourself when nodes are getting ready
-  elif $KUBECTL get nodes 2>/dev/null | grep NotReady >/dev/null; then
-    sleep 5 && echo Waiting for Kubernetes nodes to be ready
+  elif $KUBECTL get nodes >/dev/null 2>&1; then
+    NODES=$($KUBECTL get nodes -o json 2>/dev/null | jq -r '.items[].metadata.name' | wc -l)
+    NODES_READY=$($KUBECTL get nodes -o json 2>/dev/null | jq -r '.items[].status.conditions[] | select(.reason == "KubeletReady") | .type' | wc -l)
+    if ((NODES_READY == NODES)); then
+      break
+    else
+      sleep 5 && echo Waiting for Kubernetes nodes to be ready
+    fi
   fi
 done
